c874a29e5c631a0bcc7f467b471e7127c576a594
Muthukumar <msundararajan@paypal.com>
2015-11-28 13:47:26 +0530

    Basic working app Ready!!!

--- paymyfee-rest/app.js
+++ paymyfee-rest/app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({
 ----------------------------------------------------------------------------*/
 app.get('/v1/payMyFee', function(req, res) {
 
+    console.log('Reached Service');
 
 });
 module.exports = app;
