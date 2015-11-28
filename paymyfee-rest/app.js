426436587f600354a5fcff072cc51f56eee52721
svaithiyanathan <svaithiyanathan@paypal.com>
2015-11-28 19:41:27 +0530

    Merged

--- paymyfee-rest/app.js
+++ paymyfee-rest/app.js
@@ -9,6 +9,10 @@ var config = require('./config');
 var Err = require('./lib/Err');
 var ErrCodes = require('./lib/ErrCodes');
 var RecipientService = require('./lib/RecipientService')(config.dbcp);
+var dust = require('express-dustjs');
+var path = require('path');
+
+
 // for parsing application/json
 app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
@@ -16,14 +20,37 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.engine('dust', dust.engine({
+    // Use dustjs-helpers
+    useHelpers: true
+}));
+
+app.set('view engine', 'dust');
+app.set('views', path.resolve(__dirname, './views'));
+
 
 /*----------------------------------------------------------------------------
  Below routes are REST API
 ----------------------------------------------------------------------------*/
+app.get('/students', function(req, res){
+
+    res.render('index',{title: 'Hello'});
+});
 
 app.get('/v1/recipients', function(req, res) {
     var options = _.pick(req.query, ['singleparent','moneyrequired','marks','city']);
     logger.info('Options =>', options);
+    RecipientService.getRecipientById(options)
+        .then(function(recipient) {
+            res.json(recipient);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+
+app.post('/v1/recipients', function(req, res) {
 
     RecipientService.getRecipients(options)
         .then(function(recipient) {
@@ -37,12 +64,12 @@ app.get('/v1/recipients/:id', function(req, res) {
     var options = _.pick(req.params,['id']);
     logger.info('Options =>', options);
     RecipientService.getRecipientById(options)
-        .then(function(recipient) {
-            res.json(recipient);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
+      .then(function(recipient) {
+          res.json(recipient);
+      })
+      .catch(function(err) {
+          res.json(err);
+      });
 });
 
 
